c52a6288f6441328948d2c9d0fba59ea12d18e62
Benny Baumann <BenBE@geshi.org>
2025-09-03 21:05:56 +0200

    Implement default handling for MEI request

--- common/modbus_write.c
+++ common/modbus_write.c
@@ -1,9 +1,48 @@
 #include "modbus.h"
 
+#include <string.h>
+
 #include "stm32f0xx_ll_usart.h"
 
 #include "modbus_crc.h"
+#include "version.h"
 
+
+typedef struct devinfo_record {
+    uint8_t object_id;
+    size_t object_length;
+    uint8_t *object_data;
+} devinfo_record_t;
+
+static const devinfo_record_t device_info[] = {
+    {
+        .object_id = 0x00, // VendorName
+        .object_length = 0,
+        .object_data = (uint8_t *)FW_Info_Vendor,
+    },
+    {
+        .object_id = 0x01, // ProductCode
+        .object_length = 0,
+        .object_data = (uint8_t *)FW_Info_Product,
+    },
+    {
+        .object_id = 0x02, // MajorMinorRevision
+        .object_length = 0,
+        .object_data = (uint8_t *)FW_Info_Version,
+    },
+    {
+        .object_id = 0x03, // VendorUrl
+        .object_length = 0,
+        .object_data = (uint8_t *)FW_Info_Homepage,
+    },
+
+    // End of List
+    {
+        .object_id = 0,
+        .object_length = 0,
+        .object_data = NULL,
+    },
+};
 
 static void mb_send_buffer(uint8_t *buf, uint8_t len) {
     for (uint8_t i = 0; i < len; i++) {
@@ -42,12 +81,109 @@ static void mb_send_packet(mb_packet *packet) {
     mb_send_buffer(buf, len);
 }
 
+static void mb_packet_handle_mei(mb_packet *packet) {
+    if (packet->address != MB_SLAVE_ADDRESS) {
+        return;
+    }
+
+    // Handle MEI (More Extended Information) function
+    if (packet->function != MB_FUNC_MEI) {
+        // Invalid function code for MEI
+        mb_send_error(packet->address, packet->function, MB_EX_ILLEGAL_FUNCTION);
+        return;
+    }
+
+    if (packet->data_len < 3) {
+        // Invalid data length
+        mb_send_error(packet->address, packet->function, MB_EX_ILLEGAL_DATA_VALUE);
+        return;
+    }
+
+    uint8_t mei_type = packet->data[0];
+    if (mei_type != 0x0E) {
+        // Unsupported MEI type
+        mb_send_error(packet->address, packet->function, MB_EX_ILLEGAL_DATA_VALUE);
+        return;
+    }
+
+    // Read parameters we need to respond
+    uint16_t read_device_id_code = packet->data[1];
+    if (read_device_id_code < 1 || read_device_id_code > 4) {
+        // Invalid Read Device ID code
+        mb_send_error(packet->address, packet->function, MB_EX_ILLEGAL_DATA_VALUE);
+        return;
+    }
+
+    uint8_t max_id = 0xFF;
+    if (read_device_id_code == 1) {
+        max_id = 0x02; // Basic
+    } else if (read_device_id_code == 2) {
+        max_id = 0x2A; // Regular
+    } else if (read_device_id_code == 3) {
+        max_id = 0xFF; // Extended
+    } // read_device_id_code == 4 -> All
+
+    uint8_t read_object_id = packet->data[2];
+    size_t device_info_index = 0;
+    while (!!device_info[device_info_index].object_data) {
+        if (device_info[device_info_index].object_id > max_id) {
+            // Jump to the end of the list
+            while(!!device_info[device_info_index].object_data) {
+                device_info_index++;
+            }
+            break;
+        }
+
+        if (device_info[device_info_index].object_id == read_object_id) {
+            break;
+        }
+        device_info_index++;
+    }
+    if (!device_info[device_info_index].object_data) {
+        if (read_device_id_code == 4) {
+            // No more objects
+            mb_send_error(packet->address, packet->function, MB_EX_ILLEGAL_DATA_ADDRESS);
+            return;
+        }
+        device_info_index = 0;
+    }
+
+    bool has_more = !!device_info[device_info_index+1].object_data;
+    if (read_device_id_code == 4) {
+        has_more = false;
+    }
+
+    size_t object_length = device_info[device_info_index].object_length;
+    if (!object_length) {
+        object_length = strlen((char*)device_info[device_info_index].object_data);
+    }
+
+    // Prepare response packet
+    packet->data[0] = 0x0E; // MEI type
+    packet->data[1] = read_device_id_code; // Read Device ID code
+    packet->data[2] = 0x83; // Conformity level (Extended + Individual)
+    packet->data[3] = has_more ? 0xFF : 0x00;
+    packet->data[4] = has_more ? device_info[device_info_index+1].object_id : 0x00; // Next Object ID or 0
+    packet->data[5] = 0x01; // Number of objects
+    packet->data[6] = device_info[device_info_index].object_id; // Object ID
+    packet->data[7] = object_length; // Object Length
+    for (uint8_t i = 0; i < object_length; i++) {
+        packet->data[8 + i] = device_info[device_info_index].object_data[i];
+    }
+    packet->data_len = 8 + object_length;
+    mb_send_packet(packet);
+}
+
 void mb_packet_handle_default(mb_packet *packet) {
     if (packet->address != MB_SLAVE_ADDRESS) {
         return;
     }
 
     switch (packet->function) {
+        case MB_FUNC_MEI:
+            // Handle MEI function
+            mb_packet_handle_mei(packet);
+            break;
         default:
             // Unsupported function code
             packet->function |= 0x80; // Set exception bit
